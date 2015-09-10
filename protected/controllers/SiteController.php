2f2c367e30e0f878700e1ce544147acecdad4792
kebyang <kelvo_link@yahoo.com>
2015-09-10 23:40:24 +0800

    fixing .gitignore

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -2,7 +2,27 @@
 
 class SiteController extends Controller
 {
-
+/**
+ * Specifies the access control rules.
+ * This method is used by the 'accessControl' filter.
+ * @return array access control rules
+ */
+public function accessRules()
+{
+	return array(
+		array('allow',  // allow all users to perform 'index' and 'view' actions
+			'actions'=>array('error','login','logout'),
+			'users'=>array('*'),
+		),
+		array('allow', // allow authenticated user to perform 'create' and 'update' actions
+			'actions'=>array('index'),
+			'users'=>array('@'),
+		),
+		array('deny',  // deny all users
+			'users'=>array('*'),
+		),
+	);
+}
 	/**
 	 * This is the default 'index' action that is invoked
 	 * when an action is not explicitly requested by users.
