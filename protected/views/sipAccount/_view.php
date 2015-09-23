342dfe1e6f5481bc49c546d808447e9527cc4c94
kebyang <kelvo_link@yahoo.com>
2015-09-23 19:44:31 +0800

    status here

--- protected/views/sipAccount/_view.php
+++ protected/views/sipAccount/_view.php
@@ -14,7 +14,7 @@ if (count($data->subSipAccounts) > 0) {
 <div class="account-panels">
 	<?php
 		$this->beginWidget('zii.widgets.CPortlet', array(
-			'title'=>"<strong>".CHtml::encode($data->username)." - ".CHtml::encode($status)."</strong>",
+			'title'=>"<strong>".CHtml::encode($data->username)." - ".CHtml::encode($data->vicidial_identification)."</strong>",
 			'titleCssClass'=>''
 		));
 	?>
