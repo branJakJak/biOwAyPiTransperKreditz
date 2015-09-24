f6e43b3b1d5b68bb86cb2f6b88bf474f32867005
kebyang <kelvo_link@yahoo.com>
2015-09-24 17:33:44 +0800

    checking exact balance

--- protected/views/subSipAccount/admin.php
+++ protected/views/subSipAccount/admin.php
@@ -50,6 +50,7 @@ or <b>=</b>) at the beginning of each of your search values to specify how the c
 		'username',
 		'password',
 		'last_checked_bal',
+		'exact_balance',
 		'date_created',
 		'date_updated',
 		array(
