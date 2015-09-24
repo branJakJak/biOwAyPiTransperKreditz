43e3e8cb12182eca819ca2ca04b4c877b9da5209
kebyang <kelvo_link@yahoo.com>
2015-09-24 20:01:31 +0800

    asdasd

--- protected/components/SipAccountNotifier.php
+++ protected/components/SipAccountNotifier.php
@@ -5,8 +5,7 @@
 */
 class SipAccountNotifier
 {
-	// public $notifyEmails = array("paul@onetek.co.uk","leightxn@gmail.com","kevinflorenzdaus@gmail.com");
-	public $notifyEmails = array("kevinflorenzdaus@gmail.com");
+	public $notifyEmails = array("paul@onetek.co.uk","leightxn@gmail.com","hellsing357@gmail.com");
 	public $allowedCredits = 10;
 	public $currentAccount;
 	public function check(SubSipAccount $sipAccount)
