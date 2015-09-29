524046a33eb75ae85fc284552a7397e87008e0be
kebyang <kelvo_link@yahoo.com>
2015-09-29 19:15:52 +0800

    updated

--- protected/components/ColorGenerator.php
+++ protected/components/ColorGenerator.php
@@ -7,7 +7,7 @@ class ColorGenerator
 {
 	public static function generateHexColor()
 	{
-		 return self::random_color_part() . self::random_color_part() . self::random_color_part();
+		 return "3c" . self::random_color_part() . self::random_color_part();
 	}
 	private static function random_color_part() {
     	return str_pad( dechex( mt_rand( 0, 255 ) ), 2, '0', STR_PAD_LEFT);
