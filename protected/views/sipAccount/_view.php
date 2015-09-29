072c89447ee5996026f93e3e982d32465ece5a0c
kebyang <kelvo_link@yahoo.com>
2015-09-30 00:11:55 +0800

    removed small tag

--- protected/views/sipAccount/_view.php
+++ protected/views/sipAccount/_view.php
@@ -14,7 +14,7 @@ if (count($data->subSipAccounts) > 0) {
 <div class="account-panels">
 	<?php
 		$this->beginWidget('zii.widgets.CPortlet', array(
-			'title'=>"<strong>".CHtml::encode($data->username)." - ".CHtml::encode($data->getStatusLabel())."</strong> - ".(!empty($data->vicidial_identification) ? CHtml::encode($data->vicidial_identification) : "<small>No vici identification</small>"),
+			'title'=>"<strong>".CHtml::encode($data->username)." - ".CHtml::encode($data->getStatusLabel())."</strong> - ".(!empty($data->vicidial_identification) ? CHtml::encode($data->vicidial_identification) : "No vici identification"),
 			'titleCssClass'=>''
 		));
 	?>
