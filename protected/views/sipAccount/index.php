2098f37de24a2836289f1e2c33ff60be4129bf45
kebyang <kelvo_link@yahoo.com>
2015-10-07 03:19:12 +0800

    sdasd

--- protected/views/sipAccount/index.php
+++ protected/views/sipAccount/index.php
@@ -193,7 +193,7 @@ $this->widget('bootstrap.widgets.TbAlert', array(
 </table>
 <hr>
 <button ng-cloak type="button" class="btn btn-default" ng-click="indexCtrl.globalUpdate()">
-	<i class="fa fa-spinner fa-spin" ng-show="globalUpdateText === 'Updating data...' "></i>
+	<i class="fa fa-spinner fa-spin" ng-show="globalUpdateText === 'Updating data...' || globalUpdateText === 'Loading data...' "></i>
 	{{globalUpdateText}} {{updateDataReport}}
 </button>
 
