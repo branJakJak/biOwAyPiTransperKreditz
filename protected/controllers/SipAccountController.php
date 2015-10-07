796bdc1ca8e08be5b5a1c3e693b9d89f2656e64a
kebyang <kelvo_link@yahoo.com>
2015-10-07 23:50:51 +0800

    ring test

--- protected/controllers/SipAccountController.php
+++ protected/controllers/SipAccountController.php
@@ -300,8 +300,8 @@ class SipAccountController extends Controller
         header("Content-Type: application/json");
         $jsonMessage = array("succes"=>false,"message"=>"Incomplete parameter/data");
         $notifier = new SipAccountNotifier();
-        // $notifier->quickRing();
-        mail("hellsing357@gmail.com", "ring test", "this is a ring test");
+        $notifier->quickRing();
+        // mail("hellsing357@gmail.com", "ring test", "this is a ring test");
     }
 
     /**
