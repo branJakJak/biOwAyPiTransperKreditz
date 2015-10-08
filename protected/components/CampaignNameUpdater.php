ffc01d8f5a3dda0fb8185129d5b1b15d528dd097
kebyang <kelvo_link@yahoo.com>
2015-10-09 00:35:30 +0800

    fixing bind parameters

--- protected/components/CampaignNameUpdater.php
+++ protected/components/CampaignNameUpdater.php
@@ -10,12 +10,10 @@ class CampaignNameUpdater
 			UPDATE user_carrier SET campaign=:newCampaign 
 			where vici_user = :viciUser and main_user = :mainUser and main_pass = :mainPass
 		");
-		$command->params = array(
-				"newCampaign"=>$newCampaignValue,
-				"viciUser"=>$viciUser,
-				"mainUser"=>$mainUser,
-				"mainPass"=>$mainPass
-			);
+		$command->bindParam(":newCampaign",$newCampaignValue,PDO::PARAM_STR);
+		$command->bindParam(":viciUser",$viciUser,PDO::PARAM_INT);
+		$command->bindParam(":mainUser",$mainUser,PDO::PARAM_STR);
+		$command->bindParam(":mainPass",$mainPass,PDO::PARAM_STR);
 		return $command->execute();
 	}
 }
