f89d2c36ab57e5917c104bc19353b4239bf89aa6
kebyang <kelvo_link@yahoo.com>
2015-10-09 00:47:39 +0800

    some parameter typo shit

--- protected/components/CampaignNameUpdater.php
+++ protected/components/CampaignNameUpdater.php
@@ -10,10 +10,16 @@ class CampaignNameUpdater
 			UPDATE user_carrier SET campaign=:newCampaign 
 			where vici_user = :viciUser and main_user = :mainUser and main_pass = :mainPass
 		");
-		$command->bindParam(":newCampaign",$newCampaignValue,PDO::PARAM_STR);
-		$command->bindParam(":viciUser",$viciUser,PDO::PARAM_INT);
-		$command->bindParam(":mainUser",$mainUser,PDO::PARAM_STR);
-		$command->bindParam(":mainPass",$mainPass,PDO::PARAM_STR);
+		$command->params = array(
+				":newCampaign" =>$newCampaignValue,
+				":viciUser" =>$viciUser,
+				":mainUser" =>$mainUser,
+				":mainPass" =>$mainPass
+			);
+		// $command->bindParam(":newCampaign",$newCampaignValue,PDO::PARAM_STR);
+		// $command->bindParam(":viciUser",$viciUser,PDO::PARAM_INT);
+		// $command->bindParam(":mainUser",$mainUser,PDO::PARAM_STR);
+		// $command->bindParam(":mainPass",$mainPass,PDO::PARAM_STR);
 		return $command->execute();
 	}
 }
