7f8ffece8cf3e6a014c1ebd071ab7107213f523d
kebyang <kelvo_link@yahoo.com>
2015-10-13 23:40:20 +0800

    updated chart js for blinking feature

--- themes/abound/js/sipAccountChart.js
+++ themes/abound/js/sipAccountChart.js
@@ -1,18 +1,32 @@
-window.updateChartData = function(data) {
-	window.chartObj.series[0].setData(data,true);
+window.chartBlink = function(){
+	dataClone = window.chartObj.series[0].data;
+	/*iterate data*/
+	jQuery.each(window.chartObj.series[0].data, function(index, val) {
+		/*if value is between 4 and 3*/
+		if (val.y <= 4 && val.y >= 3 ) {
+			if (val.color == "orange") {
+				dataClone[index].color = "yellow";
+			}else if (val.color == "yellow") {
+				dataClone[index].color = "orange";
+			}
+		}
+	});
+	window.chartObj.series[0].setData(dataClone,true);
 	jQuery.each(window.chartObj.series[0].data, function(index, val) {
 	  val.setState('hover');
 	  val.setState();
 	});
 
+}
 
-	// jQuery.ajax({
-	//   url: '/sipAccount/getBarChartReportData',
-	//   type: 'GET',
-	//   dataType: 'json',
-	//   success: function(data, textStatus, xhr) {
-	//   	window.chartObj.series[0].setData(data,true);
-	//   },
-	// });
-	
+window.updateChartData = function(data) {
+	/*flush blink interval*/
+	window.blinkerInterval = null;
+	window.chartObj.series[0].setData(data,true);
+	jQuery.each(window.chartObj.series[0].data, function(index, val) {
+	  val.setState('hover');
+	  val.setState();
+	});
+	/*start blink interval*/
+	window.blinkerInterval = setInterval(window.chartBlink, 600);
 }
