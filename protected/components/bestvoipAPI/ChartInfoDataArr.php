1e0ffb69a3ed951e6c1e69880df73a74935592cc
kebyang <kelvo_link@yahoo.com>
2015-10-21 19:08:05 +0800

    updated chart info data arr to use methods getters

--- protected/components/bestvoipAPI/ChartInfoDataArr.php
+++ protected/components/bestvoipAPI/ChartInfoDataArr.php
@@ -10,15 +10,15 @@ class ChartInfoDataArr {
         $asteriskData = AsteriskCarriers::getData();
         $voipInfoRetriever = new BestVOIPInformationRetriever();
         foreach ($asteriskData as $key => $currentAsteriskData) {
-            $xmlObject = $voipInfoRetriever->getInfo(
+            $remoteVoipResult = $voipInfoRetriever->getInfo(
                 $currentAsteriskData['main_user'],
                 $currentAsteriskData['main_pass'],
                 $currentAsteriskData['sub_user'],
                 $currentAsteriskData['sub_pass']
             );
             $currentAsteriskData['id'] = $key;
-            $currentAsteriskData['balance'] = doubleval($xmlObject->Balance);
-            $currentAsteriskData['exact_balance'] = doubleval($xmlObject->SpecificBalance);
+            $currentAsteriskData['balance'] = doubleval($remoteVoipResult->getBalance());
+            $currentAsteriskData['exact_balance'] = doubleval($xmlObject->getSpecificBalance());
             $finalArr[] = $currentAsteriskData;
         }
         return $finalArr;
