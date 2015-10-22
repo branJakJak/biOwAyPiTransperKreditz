e391ab1bb92431e1a3fc044ff0504426bb6d4616
kebyang <kelvo_link@yahoo.com>
2015-10-23 05:01:50 +0800

    final implementation of time ago

--- themes/abound/js/sipaccount.js
+++ themes/abound/js/sipaccount.js
@@ -135,15 +135,12 @@
 						/* iterate through data and set teh fresh data to sipAccounts*/
 						angular.forEach(response.data, function(freshData, index){
 
-
-							/*TODO - uncomment if prod*/
-
-							// $http.post("/sync/single",{
-							// 	'mainUsername' : freshData.main_user,
-							// 	'mainPassword' : freshData.main_pass,
-						 // 		'subUsername' : freshData.sub_user,
-						 // 		'subPassword' : freshData.sub_pass
-							// });
+							$http.post("/sync/single",{
+								'mainUsername' : freshData.main_user,
+								'mainPassword' : freshData.main_pass,
+						 		'subUsername' : freshData.sub_user,
+						 		'subPassword' : freshData.sub_pass
+							});
 
 							angular.forEach($scope.sipAccounts, function(oldData, index){
 								if (  freshData.vici_user === oldData.vici_user  ) {
