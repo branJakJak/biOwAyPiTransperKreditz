90427efaf72b02983f5983435e410cab1f001ca3
kebyang <kelvo_link@yahoo.com>
2015-10-28 21:23:18 +0800

    fixing error admin

--- protected/views/freeVoipAccounts/admin.php
+++ protected/views/freeVoipAccounts/admin.php
@@ -55,7 +55,7 @@ or <b>=</b>) at the beginning of each of your search values to specify how the c
 			'type'=>'raw',
 			'header'=>'Last update',
 			'value'=>'VoipTransDateHelper::timeAgo(strtotime($data->date_updated))'
-		)
+		),
 		array(
 			'class'=>'CButtonColumn',
 		),
