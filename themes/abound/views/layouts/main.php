564f4a7cfce856875ac5333e69a33de2129cba70
kebyang <kelvo_link@yahoo.com>
2016-02-02 03:54:26 +0800

    asd

--- themes/abound/views/layouts/main.php
+++ themes/abound/views/layouts/main.php
@@ -4,8 +4,6 @@
     <meta charset="utf-8">
     <title><?php echo CHtml::encode($this->pageTitle); ?> | <?php echo Yii::app()->name ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Free yii themes, free web application theme">
-    <meta name="author" content="Webapplicationthemes.com">
 	 <link href='//fonts.googleapis.com/css?family=Carrois+Gothic' rel='stylesheet' type='text/css'>
    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
 
