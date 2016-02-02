86011f493d396907cbe9eba88114c0f5b02276f0
kebyang <kelvo_link@yahoo.com>
2016-02-02 20:18:02 +0800

    updated freevoip

--- protected/controllers/FreeVoipAccountsController.php
+++ protected/controllers/FreeVoipAccountsController.php
@@ -37,7 +37,7 @@ class FreeVoipAccountsController extends Controller
 			),
 			array('allow', 
 				'actions'=>array('admin','delete'),
-				'user'=>array('admin'),
+				'users'=>array('admin'),
 			),
 			array('deny', 
 				'users'=>array('*'),
