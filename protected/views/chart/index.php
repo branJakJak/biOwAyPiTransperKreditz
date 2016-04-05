b5f07d8c4cf675ac5a9603e477d907cf7fe791fe
Your Name <you@example.com>
2016-04-05 23:27:40 +0800

    dsa

--- protected/views/chart/index.php
+++ protected/views/chart/index.php
@@ -95,7 +95,7 @@ setTimeout(updateChartDataInterval, 1 * 1000);
 <div id="chartContainer" style="height: 1500px"></div>
 
 <hr>
-<button type="button" class="btn btn-default btn-info btn-block" onclick="updateInformation(this)">
+<button id="updateInformationBtn" type="button" class="btn btn-default btn-info btn-block" onclick="updateInformation()">
 	Update Information
 </button>
 <?php
@@ -104,13 +104,12 @@ setTimeout(updateChartDataInterval, 1 * 1000);
 <hr>
 
 <script type="text/javascript">
-	function updateInformation(curDom) {
-		var currentDom = curDom;
-		currentDom.innerHtml = "Updating...";
+	function updateInformation() {
+		document.getElementById("updateInformationBtn").innerHtml = "Updating...";
 		setTimeout(function() {
-			currentDom.innerHtml = "Content updated";
+			document.getElementById("updateInformationBtn").innerHtml = "Content updated";
 			setTimeout(function() {
-				currentDom.innerHtml = "Update Information";
+				document.getElementById("updateInformationBtn").innerHtml = "Update Information";
 			}, 500);
 		}, 1000);
 	}
