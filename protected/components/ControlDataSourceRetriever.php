a4439898e61eb95265532add12d5bc19debc3463
Your Name <you@example.com>
2016-04-08 20:20:14 +0800

    only actives

--- protected/components/ControlDataSourceRetriever.php
+++ protected/components/ControlDataSourceRetriever.php
@@ -16,10 +16,11 @@ SELECT vicidial_remote_agents.campaign_id,
        COUNT(vicidial_remote_agents.user_start) AS agents,
        SUM(vicidial_remote_agents.number_of_lines) AS channels,
        number_of_lines
-  FROM asterisk.vicidial_remote_agents vicidial_remote_agents
+	FROM asterisk.vicidial_remote_agents vicidial_remote_agents
        INNER JOIN asterisk.vicidial_campaigns vicidial_campaigns
           ON (vicidial_remote_agents.campaign_id =
                  vicidial_campaigns.campaign_id)
+	WHERE vicidial_remote_agents.status = 'ACTIVE'
 GROUP BY vicidial_remote_agents.campaign_id		
 EOL;
 		$commandObj = Yii::app()->asterisk_db->createCommand($sqlCommand);
