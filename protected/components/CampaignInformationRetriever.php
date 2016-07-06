f3dd8fa31b9136d793afa635946052417cbd437e
Your Name <you@example.com>
2016-07-06 19:52:59 +0800

    catch the error

--- protected/components/CampaignInformationRetriever.php
+++ protected/components/CampaignInformationRetriever.php
@@ -16,12 +16,16 @@ class CampaignInformationRetriever extends CComponent
 	SELECT campaign FROM user_carrier
 	WHERE sub_user = :accountName
 EOL;
-		$sqlCommandObject = Yii::app()->asterisk_db->createCommand($sqlCommandStr);
-		$sqlCommandObject->bindParam(":accountName",$tempAccountNameContainer);
-		$result = $sqlCommandObject->queryRow();
-		if (isset($result['campaign'])) {
-			$resultContainer = $result['campaign'];
-		}else{
+		try {
+			$sqlCommandObject = Yii::app()->asterisk_db->createCommand($sqlCommandStr);
+			$sqlCommandObject->bindParam(":accountName",$tempAccountNameContainer);
+			$result = $sqlCommandObject->queryRow();
+			if (isset($result['campaign'])) {
+				$resultContainer = $result['campaign'];
+			}else{
+				$resultContainer = "Not Set";
+			}
+		} catch (Exception $e) {
 			$resultContainer = "Not Set";
 		}
 		return $resultContainer;
