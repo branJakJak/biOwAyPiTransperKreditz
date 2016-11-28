40792739f847ffb5ac81c9dd35d815c481a5a5c2
Your Name <you@example.com>
2016-11-29 02:03:32 +0800

    fixing migration error

--- protected/migrations/m161128_104239_create_scheduled_force_agent.php
+++ protected/migrations/m161128_104239_create_scheduled_force_agent.php
@@ -14,7 +14,7 @@ class m161128_104239_create_scheduled_force_agent extends CDbMigration
 			'created_at' => 'datetime',
 		    'updated_at' => 'datetime',
 		], $tableOptions);
-		$this->addForeignKey("scheduled_force_agent_fk",'scheduled_force_agent','account_id','tbl_remote_data_cache','id','CASCADE','CASCADE');
+		$this->addForeignKey("scheduled_force_agent_fk",'scheduled_force_agent','account_id','tbl_remote_data_cache','id','NO ACTION','NO ACTION');
 	}
 
 	public function down()
