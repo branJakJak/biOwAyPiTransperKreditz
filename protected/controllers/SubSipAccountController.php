709390b3a1cc19db060cd57fdde857267884aab0
kevin daus <kevinflorenzdaus@gmail.com>
2017-02-04 04:07:58 +0800

    fixing bug comparing boolean(single result returned)

--- protected/controllers/SubSipAccountController.php
+++ protected/controllers/SubSipAccountController.php
@@ -110,7 +110,10 @@ class SubSipAccountController extends Controller {
         $sipAccountNames = array();
         $chartSeriesData = array();
         $criteria = new CDbCriteria;
-        $criteria->compare("is_hidden",0);//show only the visible account
+        // $criteria->compare("is_hidden",0);//show only the visible account
+        $criteria->addColumnCondition(array(
+            'is_hidden' => false,
+        ));        
         // $criteria->condition = "balance > 0";
         
         $criteria->order = "vici_user ASC";
@@ -149,7 +152,9 @@ class SubSipAccountController extends Controller {
         $formModel = new TopupForm;
         $criteria = new CDbCriteria();
         // $criteria->condition = "balance > 0";
-        $criteria->compare("is_hidden",0);//show only the visible account
+        $criteria->addColumnCondition(array(
+            'is_hidden' => false,
+        ));        
         $tempCon123 = RemoteDataCache::model()->findAll($criteria);
         $numberOfAccounts = count($tempCon123);
         $topupLogsTotalToday = 0;
@@ -215,7 +220,10 @@ class SubSipAccountController extends Controller {
         $accountsCollection = [];
         $criteria = new CDbCriteria();
         // $criteria->condition = "balance > 0";
-        $criteria->compare("is_hidden",0);//show only the visible account
+        // $criteria->compare("is_hidden",0);//show only the visible account
+        $criteria->addColumnCondition(array(
+            'is_hidden' => false,
+        ));                
         $tempContainer = RemoteDataCache::model()->findAll($criteria);
         foreach ($tempContainer as $key => $currentRemoteDataCache) {
             $accountsCollection[] = $currentRemoteDataCache->sub_user;
