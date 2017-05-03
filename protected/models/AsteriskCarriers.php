dde1bf9351dd8705a612c16e9af5ddc71d57dc8a
kevin daus <kevinflorenzdaus@gmail.com>
2017-05-03 22:31:14 +0800

    get all user carrier

--- protected/models/AsteriskCarriers.php
+++ protected/models/AsteriskCarriers.php
@@ -6,7 +6,8 @@
 class AsteriskCarriers {
 
     public static function getData() {
-        $command = Yii::app()->asterisk_db->createCommand("select * from user_carrier where server_ip <> '69.10.58.29' ");
+        // $command = Yii::app()->asterisk_db->createCommand("select * from user_carrier where server_ip <> '69.10.58.29' ");
+        $command = Yii::app()->asterisk_db->createCommand("select * from user_carrier");
         return $command->queryAll();
     }
 
